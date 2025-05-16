debf62ddba9eb2839e2cb8e225a3121ad04023be
ttd11204 <datkk112004@gmail.com>
2025-05-16 22:16:29 +0700

    aloha init

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import './global.css';
+
+export default function Index() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-5xl text-accent '>WELCOME.</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#030014',
+        accent: '#AB8BFF',
+      }
+    },
+  },
+  plugins: [],
+}
